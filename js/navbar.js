2145113b1a60d41e4d48eec619e727806c06e93e
Ekuspreet <ekuspreetsingh2@gmail.com>
2025-10-15 10:42:46 +0530

    buigfix: fixed active issue

--- js/navbar.js
+++ js/navbar.js
@@ -9,7 +9,8 @@ const nav_id_map = {
   gallery: "gallery",
   contact: "contact-us",
 };
-var current_path = window.location.pathname.split("/")[0] || "";
+var current_path = window.location.pathname.split("/").pop();
+console.log(current_path);
 current_path = current_path.replace(".html", "");
 const nav_element = document.getElementById(nav_id_map[current_path]);
 console.log(current_path, nav_element);
