639e13c7ad884ebe37e9a6385d4926b33d05492f
Ekuspreet <ekuspreetsingh2@gmail.com>
2025-10-15 10:22:03 +0530

    build: added all pages

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+const nav_id_map = {
+  "": "home",
+  about: "about-us",
+  cfp: "call-for-papers",
+  submission: "submission",
+  speakers: "invited-speakers",
+  committees: "committee",
+  registration: "registration",
+  gallery: "gallery",
+  contact: "contact-us",
+};
+const current_path = window.location.pathname.split("/")[1] || "";
+const nav_element = document.getElementById(nav_id_map[current_path]);
+console.log(current_path, nav_element);
+if (nav_element) {
+  console.log(nav_id_map[current_path]);
+  nav_element.classList.add("border-white", "hover:border-white");
+}
