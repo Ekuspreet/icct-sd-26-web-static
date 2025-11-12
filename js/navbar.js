f84359cf3d75214d6c9f61d0816981b6bf0652eb
Ekuspreet <ekuspreetsingh2@gmail.com>
2025-11-12 11:44:35 +0530

    changed core websites colors

--- js/navbar.js
+++ js/navbar.js
@@ -15,5 +15,10 @@ var current_path = window.location.pathname.split("/").pop();
 current_path = current_path.replace(".html", "");
 const nav_element = document.getElementById(nav_id_map[current_path]);
 if (nav_element) {
-  nav_element.classList.add("border-white", "hover:border-white");
+  nav_element.classList.add(
+    "border-white",
+    "border-2",
+    "font-black",
+    "hover:border-white"
+  );
 }
